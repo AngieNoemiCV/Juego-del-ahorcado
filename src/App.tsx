d3ad4883b07f502effb788eaf4fc7ce779ca5a42
AngieNoemiCV <20460159@colima.tecnm.mx>
2024-05-28 14:24:37 -0600

    Cambiof

--- src/App.tsx
+++ src/App.tsx
@@ -2,54 +2,49 @@ import { useState } from "react";
 import Welcome from './components/Welcome';
 import Hangman from './components/Hangman';
 
-const frutas = ["manzana", "banana", "cereza", "mandarina", "higo", "uva", "kiwi"];
-const figuras = ["triangulo", "cuadrado", "rectangulo", "hexagono", "circulo"];
-const colores = ["azul", "rosa", "morado", "cafe", "negro", "blanco", "gris"];
-const electrodomesticos = ["television", "refrigerador", "lavadora", "microondas", "estufa"];
-
-const words = [frutas,figuras,colores,electrodomesticos]
-const pistas = {
-    frutas: {
-        manzana: "Es una fruta comúnmente asociada con dispositivos electrónicos.",
-        banana: "Es una fruta amarilla y alargada que viene en racimos.",
-        cereza: "Es una pequeña fruta roja o negra con un hueso en el centro.",
-        mandarina: "Soy una fruta pequeña y redonda, mi piel es fácil de pelar sin ninguna fricción. Dentro de mí, encontrarás segmentos jugosos y dulces, que te harán sonreír con cada bocado. ¿Quién soy?",
-        higo: "Es una fruta de forma ovalada con una piel suave y una textura carnosa.",
-        uva: "Es una pequeña fruta que crece en racimos y se utiliza para hacer vino.",
-        kiwi: "Es una fruta pequeña y peluda, de color marrón por fuera y verde por dentro."
-    },
-    figuras: {
-        triangulo: "Es una figura con tres lados y tres ángulos.",
-        cuadrado: "Es una figura con cuatro lados de igual longitud y cuatro ángulos rectos.",
-        rectangulo: "Es una figura con cuatro lados y cuatro ángulos, donde los ángulos opuestos son iguales.",
-        hexagono: "Es una figura con seis lados y seis ángulos.",
-        circulo: "Es una figura plana con todos los puntos equidistantes del centro."
-    },
-    colores: {
-        azul: "Es el color del cielo y el mar, y se asocia con la calma y la serenidad.",
-        rosa: "Es un color suave que se asocia con la feminidad y la delicadeza.",
-        morado: "Es un color entre azul y rojo, y se asocia con la realeza y la espiritualidad.",
-        cafe: "Es un color oscuro que se asemeja al color del café tostado.",
-        negro: "Es el color más oscuro y se asocia con el misterio y la elegancia.",
-        blanco: "Es el color de la pureza y se asocia con la inocencia y la paz.",
-        gris: "Es un color intermedio entre blanco y negro, y se asocia con la neutralidad y la sobriedad."
-    },
-    electrodomesticos: {
-        television: "Es un dispositivo electrónico que muestra imágenes y sonido a través de ondas.",
-        refrigerador: "Es un electrodoméstico que se utiliza para mantener los alimentos frescos y fríos.",
-        lavadora: "Es un electrodoméstico utilizado para lavar la ropa.",
-        microondas: "Es un electrodoméstico que se utiliza para calentar alimentos rápidamente.",
-        estufa: "Es un electrodoméstico utilizado para cocinar alimentos mediante el calor."
-    }
-};
+const pistas:any[] = [
+     [
+        {palabra:"manzana",pista: "Es una fruta comúnmente asociada con dispositivos electrónicos."},
+{palabra:"banana",pista: "Es una fruta amarilla y alargada que viene en racimos."},
+{palabra:"cereza",pista: "Es una pequeña fruta roja o negra con un hueso en el centro."},
+{palabra:"mandarina",pista: "Soy una fruta pequeña y redonda, mi piel es fácil de pelar sin ninguna fricción. Dentro de mí, encontrarás segmentos jugosos y dulces, que te harán sonreír con cada bocado. ¿Quién soy?"},
+{palabra:"higo",pista: "Es una fruta de forma ovalada con una piel suave y una textura carnosa."},
+{palabra:"uva",pista: "Es una pequeña fruta que crece en racimos y se utiliza para hacer vino."},
+{palabra:"kiwi",pista: "Es una fruta pequeña y peluda, de color marrón por fuera y verde por dentro."}
+    ],
+     [
+        {palabra:"triangulo",pista: "Es una figura con tres lados y tres ángulos."},
+        {palabra:"cuadrado",pista: "Es una figura con cuatro lados de igual longitud y cuatro ángulos rectos."},
+        {palabra:"rectangulo",pista: "Es una figura con cuatro lados y cuatro ángulos, donde los ángulos opuestos son iguales."},
+        {palabra:"hexagono",pista: "Es una figura con seis lados y seis ángulos."},
+        {palabra:"circulo",pista: "Es una figura plana con todos los puntos equidistantes del centro."}
+    ],
+     [
+        {palabra:"azul", pista: "Es el color del cielo y el mar, y se asocia con la calma y la serenidad."},
+        {palabra:"rosa", pista: "Es un color suave que se asocia con la feminidad y la delicadeza."},
+        {palabra:"morado", pista: "Es un color entre azul y rojo, y se asocia con la realeza y la espiritualidad."},
+        {palabra:"cafe", pista: "Es un color oscuro que se asemeja al color del café tostado."},
+        {palabra:"negro", pista: "Es el color más oscuro y se asocia con el misterio y la elegancia."},
+        {palabra:"blanco", pista: "Es el color de la pureza y se asocia con la inocencia y la paz."},
+        {palabra:"gris", pista: "Es un color intermedio entre blanco y negro, y se asocia con la neutralidad y la sobriedad."}
+    ],
+     [
+        {palabra:"television",pista: "Es un dispositivo electrónico que muestra imágenes y sonido a través de ondas."},
+        {palabra:"refrigerador",pista: "Es un electrodoméstico que se utiliza para mantener los alimentos frescos y fríos."},
+        {palabra:"lavadora",pista: "Es un electrodoméstico utilizado para lavar la ropa."},
+        {palabra:"microondas",pista: "Es un electrodoméstico que se utiliza para calentar alimentos rápidamente."},
+        {palabra:"estufa",pista: "Es un electrodoméstico utilizado para cocinar alimentos mediante el calor."}
+    ]
+];
 
 function App() {
     const [start, setStart] = useState(false);
-    const random = Math.floor(Math.random() * words.length);
+    const random = Math.floor(Math.random() * pistas.length);
+    const pista = pistas[random];
     return (
         <div className='App'>
             {start ? (
-                <Hangman words={words[random]} pista={pistas[Object.keys(pistas)[random]]} />
+                <Hangman words={pista}  />
             ) : (
                 <Welcome onStart={() => setStart(true)} />
             )}
--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -3,8 +3,7 @@ import Reloj from './Reloj';
 
 
 interface HangmanProps {
-    words: string[];
-    pista: { [word: string]: string };
+    words: {palabra:string,pista: string}[];
 }
 
 import error1 from '../img/Error1.png'
@@ -16,13 +15,13 @@ import error6 from '../img/Error6.png'
 import error7 from '../img/Error7.png'
 
 const Errores = [error1,error2,error3,error4,error5,error6,error7]
-const Hangman = ({ words, pista }: HangmanProps) => { 
+const Hangman = ({ words }: HangmanProps) => { 
 
     const [selectedWords, setSelectedWord] = useState(words[0]);
     const [guessdLetters, setGuessedLetters] = useState<string[]>([]);
     const [errorCount, setErrorCount] = useState(0);
 
-    const displayWord = selectedWords.split('').map((letter) => {
+    const displayWord = selectedWords.palabra.split('').map((letter) => {
         console.log("SelectWord: ", selectedWords)
         if (guessdLetters.includes(letter)) {
             console.log("guesedLetters: ", guessdLetters)
@@ -35,7 +34,7 @@ const Hangman = ({ words, pista }: HangmanProps) => {
     const handleGuess = (letter: string) => {
         if (!guessdLetters.includes(letter)) {
             setGuessedLetters([...guessdLetters, letter]);
-            if (!selectedWords.includes(letter)) {
+            if (!selectedWords.palabra.includes(letter)) {
                 setErrorCount((prev) => prev + 1);
                 console.log("setErrorCount: ", setErrorCount)
             }
@@ -55,12 +54,12 @@ const Hangman = ({ words, pista }: HangmanProps) => {
         <div>
             <p>{displayWord.join(' ')}</p>
             <Reloj intervalo={1000} /> 
-            <p>Pista: {pista[selectedWords]}</p>
+            <p>Pista: {selectedWords.pista}</p>
             <img className="imagenFases" src={Errores[errorCount]} />
             
             <input maxLength={1} onChange={(e) => handleGuess(e.target.value)} />
             {
-                (displayWord.join('') === selectedWords || errorCount > 5) && (
+                (displayWord.join('') === selectedWords.palabra || errorCount > 5) && (
                     <button onClick={() => {
                         restartGame();
                         setSelectedWord(words[Math.floor(Math.random() * words.length)]);
@@ -68,7 +67,7 @@ const Hangman = ({ words, pista }: HangmanProps) => {
                 )
             }
             <p>Cantidad de errors: { errorCount }</p>
-            {displayWord.join('') === selectedWords && (
+            {displayWord.join('') === selectedWords.palabra && (
 
                 <p>You win in this game</p>
 
